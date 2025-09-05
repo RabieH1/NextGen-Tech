7678d986312a028c705e8ecb10257f0234b0e0c5
Rabie <73874115+RabieH1@users.noreply.github.com>
2025-09-05 10:49:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// Bouton WhatsApp
-function openWhatsApp() {
-  const phone = "33662188467"; // Numéro sans le 0 initial ni le +
-  const message = encodeURIComponent("Bonjour, je suis intéressé par vos services informatiques.");
-  const url = `https://wa.me/${phone}?text=${message}`;
-  window.open(url, "_blank");
-}
-
-// Scroll fluide vers la section "Contact" si bouton "Get Started"
+// Scroll fluide pour les ancres internes
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -16,3 +8,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   });
 });
+
+// Ouvrir une conversation WhatsApp
+function openWhatsApp() {
+  const phone = "33662188467"; // Numéro sans le 0 initial ni le +
+  const message = encodeURIComponent("Bonjour, je suis intéressé par vos services informatiques.");
+  const url = `https://wa.me/${phone}?text=${message}`;
+  window.open(url, "_blank");
+}
